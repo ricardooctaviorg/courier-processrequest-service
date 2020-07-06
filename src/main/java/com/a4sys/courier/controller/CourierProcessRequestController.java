9db5818741e40873e49df3f9ad0024c6c1949e4d
gibranGutierrezJuarez <ggutierrez@a4sys.com>
2020-07-06 16:42:27 -0500

    add new feature-> log4j

--- src/main/java/com/a4sys/courier/controller/CourierProcessRequestController.java
+++ src/main/java/com/a4sys/courier/controller/CourierProcessRequestController.java
@@ -14,12 +14,12 @@ import com.a4sys.courier.beans.CourierProcessResponse;
 import com.a4sys.courier.domain.cfg.DicDiccionarios;
 import com.a4sys.courier.service.ICreateCourierRequestInvoker;
 import com.a4sys.courier.service.IDicDiccionariosService;
-
+import org.apache.log4j.Logger;
 
 @RestController
 @RequestMapping("/")
 public class CourierProcessRequestController implements ICourierProcessRequestController {
-	
+	final static Logger logger = Logger.getLogger(CourierProcessRequestController.class);
 	@Autowired
 	//inyecccion de dependencia
 	private IDicDiccionariosService iDicDiccionariosService;
@@ -32,7 +32,24 @@ public class CourierProcessRequestController implements ICourierProcessRequestCo
 	@PostMapping
 	@RequestMapping(value = "/courierProcessRequest", method = RequestMethod.POST)
 	public CourierProcessResponse courierProcessRequest(@RequestBody CourierProcessRequest courierProcessRequest){
+		runMe("Log de prueba");
 		CourierProcessResponse courierProcessResponse = iCreateCourierRequestInvoker.createCourierRequest(courierProcessRequest);
 		return courierProcessResponse;
+	}
+	
+	private void runMe(String parameter){
+
+		if(logger.isDebugEnabled()){
+			logger.debug("This is debug : " + parameter);
+		}
+
+		if(logger.isInfoEnabled()){
+			logger.info("This is info : " + parameter);
+		}
+
+		logger.warn("This is warn : " + parameter);
+		logger.error("This is error : " + parameter);
+		logger.fatal("This is fatal : " + parameter);
+
 	}
 }
